ba83a5fa0219667f233d92382eff8f368c08d049
Peter Ahlgren <ahlgren1234@gmail.com>
2017-12-05 13:31:02 +0100

    Initial Commit

---
+++ PAModalStatus/PAModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  PAModalStatus.h
+//  PAModalStatus
+//
+//  Created by Peter Ahlgren on 2017-12-05.
+//  Copyright © 2017 Peter Ahlgren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PAModalStatus.
+FOUNDATION_EXPORT double PAModalStatusVersionNumber;
+
+//! Project version string for PAModalStatus.
+FOUNDATION_EXPORT const unsigned char PAModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PAModalStatus/PublicHeader.h>
+
+
