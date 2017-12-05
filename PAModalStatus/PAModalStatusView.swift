4d308f553df931f2e815ea17a7547d3d0dffc8e3
Peter Ahlgren <ahlgren1234@gmail.com>
2017-12-05 13:59:43 +0100

    Initial Commit

---
+++ PAModalStatus/PAModalStatusView.swift
@@ -0,0 +1,21 @@
+//
+//  PAModalStatusView.swift
+//  PAModalStatus
+//
+//  Created by Peter Ahlgren on 2017-12-05.
+//  Copyright © 2017 Peter Ahlgren. All rights reserved.
+//
+
+import UIKit
+
+class PAModalStatusView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
